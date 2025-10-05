5c65fd5889287f1a42a3177bb38893bb35400747
Frederic Germain <frederic.germain@global.com>
2025-10-05 17:05:39 +0100

    update setup.py - update setuptools, PEP 639 license declaration style
    setuptools 70 mandates PEP 639 - license declaration style
    
    License is still clearly described as MIT

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ from pathlib import Path
 from setuptools import find_namespace_packages, setup
 
 dirname = Path(os.path.dirname(__file__))
-version = "v1.1.0"
+version = "1.1.0"
 
 requirements = {
     "install": [
-        "setuptools<=65.0",
+        "setuptools",
         "numpy>=1.24.4",
         "kaldi_native_fbank",
         "onnxruntime>=1.17.0",
@@ -39,7 +39,7 @@ setup(
     "with onnxruntime",
     long_description=open(os.path.join(dirname, "README.md"), encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    license="The MIT License",
+    license="MIT", # SPDX identifier
     packages=find_namespace_packages(),
     include_package_data=True,
     install_requires=install_requires,
@@ -55,7 +55,6 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Science/Research",
         "Operating System :: POSIX :: Linux",
-        "License :: OSI Approved :: MIT License",
         "Topic :: Multimedia :: Sound/Audio :: Speech",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Software Development :: Libraries :: Python Modules",
